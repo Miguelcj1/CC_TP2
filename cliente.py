a68dedcb0d8e28dbd04a07fa47ba6f0f606026e3
Miguelcj1 <miguelluffy7@gmail.com>
2022-11-03 16:04:32 +0000

    cominicacao

--- cliente.py
+++ cliente.py
@@ -9,7 +9,6 @@ porta = 3334 # porta qualquer acima de 1024
 msg = "Adoro Redes :)"
 for i in range(1):
     s.sendto(msg.encode('utf-8'), (endereco, porta))
-    print(endereco + " " + str(porta))
     msg, add = s.recvfrom(1024)
     print(msg.decode('utf-8'))
     print(add)
